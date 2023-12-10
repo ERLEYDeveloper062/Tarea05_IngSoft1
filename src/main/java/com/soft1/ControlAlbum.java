66057994d0714bee5e00ac2e4c4de04903891893
Luis David Rodriguez Arias <luis.1701721583@ucaldas.edu.co>
2023-12-10 17:41:18 -0500

    ControlAlbum agrego metodos

--- src/main/java/com/soft1/ControlAlbum.java
+++ src/main/java/com/soft1/ControlAlbum.java
@@ -2,6 +2,31 @@ package com.soft1;
 
 public class ControlAlbum {
 
-    
+    private List<Album> albumes;
+
+    public ControlAlbum() {
+        this.albumes = new ArrayList<>();
+    }
+
+    public Banda buscarAlbum(String nombreAlbum) {
+        return albumes.stream()
+                .filter(album -> albumes.getNombre().equals(nombreAlbum))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Banda buscarCancion(String nombreCancion) {
+        return canciones.stream()
+                .filter(cancion -> cancion.getNombre().equals(nombreCancion))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void agregarCanciones(String nombreCancion, String nombreAlbum){
+        private Cancion cancion = buscarCancion(nombreCancion);
+        private Album album = 
+
+        
+    }
     
 }
